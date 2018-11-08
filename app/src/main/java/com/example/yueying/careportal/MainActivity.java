d94f2240f089237d6e6a55662f916842c96403d8
Yueying Zhao <yueying.zhao@uvm.edu>
2018-11-08 00:05:21 -0500

    basic main page

--- app/src/main/java/com/example/yueying/careportal/MainActivity.java
+++ app/src/main/java/com/example/yueying/careportal/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.yueying.careportal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button help;
+    private Button news;
+    private Button health;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //dfasjdflasdkjfla
+
+        help = (Button) findViewById(R.id.help_button);
+        help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, YourActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        news = (Button) findViewById(R.id.news_button);
+        news.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, YourActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        health = (Button) findViewById(R.id.health_button);
+        health.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, YourActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
